b34ac0c308c973425a7061f2b73df899a90148aa
Mattis Hallberg <skatemattis97@hotmail.com>
2023-07-18 14:57:42 +0200

    Major changes in UI design but the logic in JS is basically the same

--- script.js
+++ script.js
@@ -57,21 +57,50 @@ function newGame() {
     numberOfPlayedRounds = 0
     humanPoints = 0
     computerPoints = 0
-    document.getElementById('round').innerHTML = numberOfPlayedRounds
-    document.getElementById('humanPoints').innerHTML = humanPoints
-    document.getElementById('computerPoints').innerHTML = computerPoints
+    document.getElementById('p1').innerHTML = 'Make your choice'
+    document.getElementById('p2').innerHTML = 'Best out of five rounds takes the win'
+    document.getElementById('final_result').innerHTML = ''
+    document.getElementById('humanChoice').innerHTML = "?"
+    document.getElementById('computerChoice').innerHTML = "?"
+    document.getElementById('humanPoints').innerHTML = 0
+    document.getElementById('computerPoints').innerHTML = 0
     document.querySelector('#rock').disabled = false
     document.querySelector('#paper').disabled = false
     document.querySelector('#scissors').disabled = false
 }
 
-function gameOver() {
+function gameOver(humanPoints, computerPoints) {
     document.querySelector('#rock').disabled = true
     document.querySelector('#paper').disabled = true
     document.querySelector('#scissors').disabled = true
-    document.getElementById('result').innerHTML = checkWinner(humanPoints, computerPoints)
-    const audio = document.querySelector('audio')
-    audio.play()
+    document.getElementById('p1').innerHTML = ''
+    document.getElementById('p2').innerHTML = ''
+    document.getElementById('final_result').innerHTML = checkWinner(humanPoints, computerPoints)
+
+    if (humanPoints < computerPoints) {
+        const audio = document.querySelector('#computerWin')
+        audio.play()
+    } else {
+        const audio = document.querySelector('#humanWin')
+        audio.play()
+    }
+}
+
+function displayChoice(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        document.getElementById('humanChoice').innerHTML = '🪨'
+    } else if (playerSelection === "paper") {
+        document.getElementById('humanChoice').innerHTML = '🧻'
+    } else if (playerSelection === "scissors") {
+        document.getElementById('humanChoice').innerHTML = '✂️'
+    }
+    if (computerSelection === "rock") {
+        document.getElementById('computerChoice').innerHTML = '🪨'
+    } else if (computerSelection === "paper") {
+        document.getElementById('computerChoice').innerHTML = '🧻'
+    } else if (computerSelection === "scissors") {
+        document.getElementById('computerChoice').innerHTML = '✂️'
+    }
 }
 
 function game() {
@@ -79,11 +108,13 @@ function game() {
     buttons.forEach((button) => {
         
         button.addEventListener('click', () => {
-            if (button.id === "playAgain") {
+            if (button.id === "play_again") {
                 newGame()
             } else {
                 const playerSelection = button.id
                 const computerSelection = getComputerChoice()
+
+                displayChoice(playerSelection, computerSelection)
 
                 const result = playRound(playerSelection, computerSelection)
 
@@ -92,11 +123,10 @@ function game() {
 
                 console.log(numberOfPlayedRounds)
 
-                document.getElementById('round').innerHTML = numberOfPlayedRounds
                 document.getElementById('humanPoints').innerHTML = humanPoints
                 document.getElementById('computerPoints').innerHTML = computerPoints
 
-                if (numberOfPlayedRounds == 3) gameOver()
+                if (numberOfPlayedRounds == 5) gameOver(humanPoints, computerPoints)
             }
             
         })
